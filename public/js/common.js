4712d4c4ef66c794b9627e78cb217f5f114bf946
PAVEL KHALUNIN <wol1414@gmail.com>
2024-09-08 18:52:24 +0200

    feat: add gsap

--- public/js/common.js
+++ public/js/common.js
@@ -59,25 +59,75 @@ function eventHandler() {
 		slideToClickedSlide: true,
 		freeModeMomentum: true,
 	});
-	const mainSlider = new Swiper(".main-slider--js", {
-		slidesPerView: 1,
-		direction: "vertical",
-		speed: 500,
+	// const mainSlider = new Swiper(".main-slider--js", {
+	// 	slidesPerView: 1,
+	// 	direction: "vertical",
+	// 	speed: 500,
+	// 	observer: true, // Enable observer
+	// 	observeParents: true, // Enable observing parent elements
 
-		effect: "creative",
-		creativeEffect: {
-			prev: {opacity: 1, translate: [0, 0, -1]},
-			next: {opacity: 1, translate: [0, "100%", 0]},
-		},
-		keyboard: {enabled: !0, onlyInViewport: !0},
-		mousewheel: {
-			sensitivity: 1,
-			thresholdDelta: 10,
-			enabled: true,
-			forceToAxis: true,
-		},
+	// 	effect: "creative",
+	// 	creativeEffect: {
+	// 		prev: {opacity: 1, translate: [0, 0, -1]},
+	// 		next: {opacity: 1, translate: [0, "100%", 0]},
+	// 	},
+	// 	keyboard: {enabled: !0, onlyInViewport: !0},
+	// 	mousewheel: {
+	// 		sensitivity: 1,
+	// 		thresholdDelta: 10,
+	// 		enabled: true,
+	// 		forceToAxis: true,
+	// 	},
+	// });
+	// mainSlider.slideTo(6);
+	const sectionCards = document.querySelectorAll(".sCards");
+	if (sectionCards) {
+		sectionCards.forEach(section => {
+			const nextParentSection = section.parentElement.nextElementSibling;
+			console.log("next", nextParentSection);
+
+			// const cards = section.querySelectorAll(".card");
+			nextParentSection.insertAdjacentHTML("afterbegin", section.outerHTML);
+		});
+	}
+
+	gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+
+	let navs = gsap.utils.toArray("nav a");
+
+	gsap.utils.toArray(".section-slide").forEach((panel, i) => {
+		let trigger = ScrollTrigger.create({
+			trigger: panel,
+			start: "top top",
+			// end: "80% bottom",
+			pin: true,
+			pinSpacing: false,
+			stagger: 0.5,
+			snap: {
+				snapTo: 0.1,
+				duration: {min: 0.1, max: 0.4},
+				ease: "easeIn",
+			},
+			// snap: true,
+			// ease: "none",
+
+			// scrub: true,
+			// end: "+=199%",
+			// onToggle: self => self.isActive && !scrollTween && goToSection(i),
+		});
+
+		let nav = navs[i];
+
+		// nav.addEventListener("click", function (e) {
+		// 	e.preventDefault();
+		// 	gsap.to(window, {
+		// 		duration: 1,
+		// 		scrollTo: trigger.start,
+		// 	});
+		// });
 	});
 }
+
 if (document.readyState !== "loading") {
 	eventHandler();
 } else {
