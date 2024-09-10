79a612786c8d38fec541b4594a239981aafacdc2
PAVEL KHALUNIN <wol1414@gmail.com>
2024-09-10 17:04:29 +0200

    feat: add swiper

--- public/js/common.js
+++ public/js/common.js
@@ -91,79 +91,26 @@ function eventHandler() {
 		});
 	}
 
-	gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+	const mainSlider = new Swiper(".main-slider--js", {
+		slidesPerView: 1,
+		direction: "vertical",
+		speed: 500,
+		autoHeight: true,
 
-	let navs = gsap.utils.toArray("nav a");
-
-	const topNav = document.querySelector(".top-nav");
-
-	gsap.utils.toArray(".section-slide").forEach((panel, i) => {
-		let trigger = ScrollTrigger.create({
-			trigger: panel,
-			start: "top top",
-			pin: true,
-			pinSpacing: false,
-			stagger: 0.5,
-			onEnter: () => {
-				console.log("onEnter", panel.classList.contains("section-slide--dark"));
-
-				if (panel.classList.contains("section-slide--dark")) {
-					topNav.classList.add("top-nav--white");
-				}
-			},
-			onLeave: () => {
-				if (panel.classList.contains("section-slide--dark")) {
-					topNav.classList.remove("top-nav--white");
-				}
-			},
-			onEnterBack: () => {
-				if (panel.classList.contains("section-slide--dark")) {
-					topNav.classList.add("top-nav--white");
-				}
-			},
-			onLeaveBack: () => {
-				if (panel.classList.contains("section-slide--dark")) {
-					topNav.classList.remove("top-nav--white");
-				}
-			},
-
-			snap: {
-				snapTo: 1,
-				duration: {min: 0.1, max: 0.4},
-				ease: "ease",
-			},
-			// snap: true,
-			// ease: "none",
-
-			// scrub: true,
-			// end: "+=199%",
-			// onToggle: self => self.isActive && !scrollTween && goToSection(i),
-		});
-
-		// gsap.from(panel, {
-		// 	// opacity: 0,
-		// 	y: 50,
-		// 	duration: 1,
-		// 	stagger: 0.2, // Delay of 0.2 seconds between each element's animation
-		// 	ease: "power2.out",
-		// 	scrollTrigger: {
-		// 		trigger: panel,
-		// 		start: "top 80%",
-		// 		end: "bottom 20%",
-		// 		toggleActions: "play none none reverse",
-		// 	},
-		// });
-
-		let nav = navs[i];
-
-		// nav.addEventListener("click", function (e) {
-		// 	e.preventDefault();
-		// 	gsap.to(window, {
-		// 		duration: 1,
-		// 		scrollTo: trigger.start,
-		// 	});
-		// });
+		effect: "creative",
+		creativeEffect: {
+			prev: {opacity: 1, translate: [0, 0, -1]},
+			next: {opacity: 1, translate: [0, "100%", 0]},
+		},
+		keyboard: {enabled: !0, onlyInViewport: !0},
+		mousewheel: {
+			sensitivity: 1,
+			thresholdDelta: 10,
+			enabled: true,
+			forceToAxis: true,
+		},
 	});
+
 	const menu = document.querySelector(".menu-mobile .menu");
 	menu.addEventListener("click", function (e) {
 		const targetLi = e.target.closest("li:has(ul)");
